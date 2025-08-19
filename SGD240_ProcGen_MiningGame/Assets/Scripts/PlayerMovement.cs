84535e57ba5432e0319dd0c6e238411cac14ce7b
KatieeeB <keb051@student.usc.edu.au>
2025-08-19 22:53:32 +1000

    Player Movement
    Set up player movement and animations.
    
    - Imported Player Sprites
    - Created idle, walk, and mining animations
    - Created PlayerMovement.cs

---
+++ SGD240_ProcGen_MiningGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private bool isFacingLeft = false;
+    private Vector2 movement;
+    private Animator animator;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody2D>(); // Get and store the rigidbody component attached to the player
+       animator = GetComponent<Animator>(); //Get and store the Animator component attached to the player
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal"); //set movement.x to horizontal input
+        movement.y = Input.GetAxisRaw("Vertical"); //set movement.y to vertical input
+
+        FlipSprite();
+
+        animator.SetFloat("Walking", Mathf.Abs(movement.sqrMagnitude)); //play walking animation when the player moves in any direction
+    }
+
+
+    void FlipSprite() //flip the sprite when the player changes direction
+    {
+        if(isFacingLeft && movement.x > 0f || !isFacingLeft && movement.x < 0f) 
+        {
+            isFacingLeft = !isFacingLeft;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime); //move player
+    }
+}
